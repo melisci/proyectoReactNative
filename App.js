a6227c45d65933a15c89d779b6244ae46fffa088
melisci <melisascime@gmail.com>
2022-09-06 10:56:17 -0300

    1 primer commit

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text/>
       <StatusBar style="auto" />
+      <View>
+      <TextInput />
+      <Button title="ADD"/>
     </View>
+    <View>
+      
+    </View>
+    </View>
+    
   );
 }
 
